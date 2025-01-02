fe2e0ce89a4bbfaf1d714c1921479e9bc321436e
yohome92 <s0934326378s@gmail.com>
2025-01-02 18:47:24 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+  delay:1500,
+},
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+$(document).ready(function() {
+    $('.fa-solid.fa-arrow-up').click(function(event){
+        $('html,body').animate({
+          scrollTop:0
+          },700)
+    });
+  });
+  
